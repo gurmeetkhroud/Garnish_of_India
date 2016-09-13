e503be2a73857a424957dcb646fd09f4e5a89c4f
Gurmeet Singh <u5438104@anu.edu.au>
2016-09-13 20:26:37 +1000

    info added

--- reserve.php
+++ reserve.php
@@ -1,3 +1,25 @@
 <?php
- // You have to download the Premium version to get working Reservation form alongwith full documentation of the  template
+if(isset($_POST['submit'])){
+    $to = "gurmeetroks@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $state = $_POST['state'];
+    $datepicker= $_POST['datepicker']
+    $phone = $_POST['phone']
+    $guest = $_POST['guest']
+    $email = $_POST['email']
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    // You cannot use header and echo together. It's one or the other.
+    }
 ?>
